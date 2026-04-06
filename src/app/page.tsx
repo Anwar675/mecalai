bad9f3c3a0ca35e5d4bada563a4a5ba9ed93eaad
anwar <doanthienan54@gmail.com>
2026-04-06 20:20:18 +0700

    set up

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const Page = () => {
+  return (
+    <Button variant="custom" className="font-bold" >
+      New Meeting
+    </Button >
+  )
+}
+
+export default Page
