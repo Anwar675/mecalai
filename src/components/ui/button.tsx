a3820fa2fee413ecca8deca67b4b8430fdfadd8c
anwar <doanthienan54@gmail.com>
2026-04-08 01:11:27 +0700

    no

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,6 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground [a]:hover:bg-primary/80",
+        custom: "bg-[#2D5169] text-white cursor-pointer [a]:hover:bg-[#2D5169]/80",
         outline:
           "border-border bg-background hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50",
         secondary:
